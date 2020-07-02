a2b83661af97f4ab67adaefbe442af3387fc1ea9
Kiwon Yoon <kiwonyoon0701@gmail.com>
2020-07-02 12:58:50 +0900

    commit

--- ssh_tunnel.sh
+++ ssh_tunnel.sh
@@ -1,4 +1,4 @@
-ssh -i /Users/kiwony/keys/id_rsa_main.pem -l ubuntu  -L 80:ubuntu.ikoocloud.com:80 ubuntu.ikoocloud.com
+ssh -i /Users/kiwony/keys/pem.pem -l ubuntu  -L 80:ubuntu.ikoocloud.com:80 ubuntu.ikoocloud.com
 
 # jupyter notebook ssh tunnel
-ssh -i /Users/kiwony/keys/id_rsa_main.pem -l ec2-user -L localhost:8888:localhost:8888 13.125.184.91
+ssh -i /Users/kiwony/keys/pem.pem -l ec2-user -L localhost:8888:localhost:8888 13.125.184.99
